26a0824bcecb9a390734fb18e18b17c2c8d7a663
Morgan Di Val <morgandival@gmail.com>
2022-03-19 16:32:04 +1100

    Ran tsc

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var path = require("path");
+var createWindow = function () {
+    var win = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.loadFile('index.html');
+};
+electron_1.app.whenReady().then(function () {
+    createWindow();
+    electron_1.app.on('activate', function () {
+        if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+electron_1.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});

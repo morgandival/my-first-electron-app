e38c3edc348e496747aa221146e99c71225a3a82
Morgan Di Val <morgandival@gmail.com>
2022-03-19 16:30:04 +1100

    Added more stuff, tsconfig

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
-const { app, BrowserWindow } = require('electron');
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   });
 
   win.loadFile('index.html');
